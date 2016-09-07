feabefd052888cd96b42d6d25915801408e6eb9f
Mats Kristian Dale <dalemk@dcsdk12.org>
2016-09-07 12:24:28 -0600

    Implement icy_hot

--- quiz.py
+++ quiz.py
@@ -12,16 +12,36 @@ def has_teen(a, b, c):
 
 	return False
 
-print has_teen(1, 1, 1)  # expect False
-print has_teen(14, 1, 1) # expect True
-print has_teen(1, 14, 1) # expect True
-print has_teen(1, 1, 14) # expect True
+print has_teen(1, 1, 1)    # expect False
+print has_teen(20, 20, 20) # expect False
+print has_teen(14, 1, 1)   # expect True
+print has_teen(1, 14, 1)   # expect True
+print has_teen(1, 1, 14)   # expect True
 print has_teen(14, 14, 14) # expect True
 print has_teen(18, 16, 14) # expect True
 
+print
 # TODO - write not_string
 
-# TODO - write icy_hot
+
+def icy_hot(x, y):
+	hot = False
+	cold = False
+
+	if x > 100 or y > 100:
+		hot = True
+
+	if x < 0 or y < 0:
+		cold = True
+
+	if cold and hot:
+		return True
+
+	return False
+
+print icy_hot(50, 50) # expect False
+print icy_hot(110, -10) # expect True
+print icy_hot(-10, 110) # expect True
 
 # TODO - write closer_to
 
