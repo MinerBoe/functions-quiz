4a94ccfc763391f2f53b5023b3fdb407c29da840
Mats Kristian Dale <dalemk@dcsdk12.org>
2016-09-07 12:07:09 -0600

    Implement has_teen

--- quiz.py
+++ quiz.py
@@ -1,5 +1,23 @@
-	
+
 # TODO - write has_teen
+def has_teen(a, b, c):
+	if a >= 13 and a <= 19:
+		return True
+
+	if b >= 13 and b <= 19:
+		return True
+
+	if c >= 13 and c <= 19:
+		return True
+
+	return False
+
+print has_teen(1, 1, 1)  # expect False
+print has_teen(14, 1, 1) # expect True
+print has_teen(1, 14, 1) # expect True
+print has_teen(1, 1, 14) # expect True
+print has_teen(14, 14, 14) # expect True
+print has_teen(18, 16, 14) # expect True
 
 # TODO - write not_string
 
